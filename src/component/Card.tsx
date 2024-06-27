b0a22832b9805cd24281dfca95d5e005615c473b
teoyo <bigbang723@naver.com>
2024-06-28 01:36:54 +0900

    update : 카드 클릭시 favorite으로 들어가는 기능(구현하려고 했는데 실패

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,25 +1,23 @@
-function Card(props) {
-  const onClickHandler = (e) => {
-    props.setSelectedCountries(e.target.value);
-    console.log(props.selectedCountries);
-  };
+import { Country } from "../types/country";
 
+interface CardProps {
+  country: Country;
+  handleSelectCountry: (country: Country) => void;
+}
+
+const Card: React.FC<CardProps> = ({ country, handleSelectCountry }) => {
   return (
-    <div className="grid grid-rows-4 grid-cols-4 m-auto">
-      {props.countries.map((country) => {
-        return (
-          <div
-            className="w-82 h-52 border-2 border-black p-4 m-8 cursor-pointer"
-            onClick={onClickHandler}
-          >
-            <img src={country.flags.png} className="mx-auto w-40 h-24" />
-            <h4 className="text-xl pt-4 pb-2">{country.name.common}</h4>
-            <p>{country.capital}</p>
-          </div>
-        );
-      })}
+    <div
+      className="w-82 h-52 border-2 border-black p-4 m-8 cursor-pointer"
+      onClick={() => {
+        handleSelectCountry;
+      }}
+    >
+      <img src={country.flags.png} className="mx-auto w-40 h-24" />
+      <h4 className="text-xl pt-4 pb-2">{country.name.common}</h4>
+      <p>{country.capital}</p>
     </div>
   );
-}
+};
 
 export default Card;
--- src/component/CountryList.tsx
+++ src/component/CountryList.tsx
@@ -1,32 +1,78 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import getCountry from "../api/api";
 import Card from "./Card";
+import { Country } from "../types/country";
 
-function CountryList() {
-  const [countries, setCountries] = useState([]);
+const CountryList: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
   const [selectedCountries, setSelectedCountries] = useState([]);
 
   useEffect(() => {
     const fetchData = async function () {
-      const data = await getCountry();
+      const data: Country[] = await getCountry();
       setCountries(data);
     };
     fetchData();
   }, []);
-  console.log(countries);
 
+  const handleSelectCountry = (country: Country): void => {
+    if (
+      !selectedCountries.find(
+        (selectedCountry: Country) =>
+          selectedCountry.name.common === country.name.common
+      )
+    ) {
+      setSelectedCountries([...selectedCountries, country]);
+    } else {
+      setSelectedCountries(
+        selectedCountries.filter(
+          (selectedCountry: Country) =>
+            selectedCountry.name.common !== country.name.common
+        )
+      );
+    }
+  };
+  console.log(selectedCountries);
   return (
     <div className="m-auto mt-5">
-      <h2 className="text-3xl py-5 text-center">Favorite Countries</h2>
-      <div></div>
-      <h2 className="text-3xl py-5 text-center">Countries</h2>
-      <Card
-        countries={countries}
-        selectedCountries={selectedCountries}
-        setSelectedCountries={setSelectedCountries}
-      />
+      <div
+        onClick={() => {
+          handleSelectCountry;
+        }}
+      >
+        <h2 className="text-3xl py-5 text-center">Favorite Countries</h2>
+        <div className="grid grid-rows-4 grid-cols-4 m-auto">
+          {selectedCountries.map((country: Country) => {
+            return (
+              <Card
+                country={country}
+                key={country.name.common}
+                handleSelectCountry={handleSelectCountry}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          handleSelectCountry;
+        }}
+      >
+        <h2 className="text-3xl py-5 text-center">Countries</h2>
+        <div className="grid grid-rows-4 grid-cols-4 m-auto">
+          {countries.map((country: Country) => {
+            return (
+              <Card
+                country={country}
+                key={country.name.common}
+                handleSelectCountry={handleSelectCountry}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default CountryList;
